50ff4a02710b0785fa6cc0e01bfc09882a3b0678
Christian Baros <ctbaros@gmail.com>
2019-04-02 12:42:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,9 @@ function destructivelyRemoveFirstKitten(name) {
 }
 
 function appendKitten(name) {
-  return[...kittens, name]
+  return[...kittens, name];
+}
+
+function prependKitten(name) {
+  return[kittens...,name]
 }
