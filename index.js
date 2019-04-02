57b1fce56206dff2244a84eb3431bbcb590a113d
Christian Baros <ctbaros@gmail.com>
2019-04-02 12:35:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var kittens = ["Milo", "Otis","Garfield"] 
-function destructivelyAppendKitten (name) {
+function destructivelyAppendKitten(name) {
   kittens.push(name)
   return kittens;
 }
 
-function destructivelyPrependKitten (name) {
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(name) {
   
 }
